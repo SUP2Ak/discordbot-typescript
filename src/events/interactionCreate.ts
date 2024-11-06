2d7ad20885ea046b79097f0725d856b0e279c3ae
SUP2Ak <cormier.wesley@gmail.com>
2024-11-06 01:51:24 +0100

    feat(interactionCreate.ts): implement event handler for interactionCreate to manage command interactions

---
+++ src/events/interactionCreate.ts
@@ -0,0 +1,19 @@
+import { CommandInteractionOptionResolver } from "discord.js";
+import { Event } from "../constructor";
+import client  from "../index";
+import { Interaction } from "@/typings";
+
+export default new Event("interactionCreate", async (interaction: any) => {
+    if (interaction.isCommand()) {
+        console.log('Command interaction detected!');
+        await interaction.deferReply();
+        const command = client.commands.get(interaction.commandName);
+        if (!command) return interaction.followUp("Command not found!");
+
+        command.run({
+            args: interaction.options as CommandInteractionOptionResolver,
+            client,
+            interaction: interaction as Interaction
+        });
+    }
+});
