51e6f2ca68367c37630223e593bcab340a86c1f5
SUP2Ak <cormier.wesley@gmail.com>
2024-11-06 16:19:44 +0100

    refactor(commands.ts): simplify command run function typing by using inline definition   refactor(commands.ts): clean up commented-out code to enhance readability and maintainability

--- src/typings/commands.ts
+++ src/typings/commands.ts
@@ -8,33 +8,16 @@ import {
 } from "discord.js";
 import { Client } from "@/constructor";
 
-/**
- * {
- *  name: "commandname",
- * description: "any description",
- * run: async({ interaction }) => {
- *
- * }
- * }
- */
 export interface Interaction extends CommandInteraction {
     member: GuildMember;
 }
-
-interface RunOptions {
-    client: Client;
-    interaction: Interaction;
-    args: CommandInteractionOptionResolver;
-}
-
-type RunFunction = (options: RunOptions) => any;
-
-export type CommandType = {
-    userPermissions?: PermissionResolvable[];
-    run: RunFunction;
-} & ChatInputApplicationCommandData;
 
 export interface RegisterCommandsOptions {
     guildId?: string;
     commands: ApplicationCommandDataResolvable[];
 }
+
+export type CommandType = {
+    userPermissions?: PermissionResolvable[];
+    run: (options: { client: Client, interaction: Interaction, args: CommandInteractionOptionResolver }) => any;
+} & ChatInputApplicationCommandData;
